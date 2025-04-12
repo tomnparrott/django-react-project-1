6e9706c7251aa483dfb2eb8ad8db7fbbf5e012c2
Thomas Parrott <tp152@student.london.ac.uk>
2025-04-12 20:13:51 +0100

    Initial Project Setup and Files - was following tutorial which turned out to be outdated - tutorial unworkable due to some aspects requiring deprecated and unsupported modules.

---
+++ DjangoAPI/EmployeeApp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Departments(models.Model):
+    DepartmentID = models.AutoField(primary_key=True)
+    DepartmentName = models.CharField(max_length=100)
+
+class Employees(models.Model):
+    EmployeeID = models.AutoField(primary_key=True)
+    EmployeeName = models.CharField(max_length=100)
+    Department = models.CharField(max_length=100)
+    DateOfJoining = models.DateField()
+    PhotoFileName = models.CharField(max_length=100)
